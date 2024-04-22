90f093812b9d482bf64602376c1887c0cbdc4004
Arif <arahman.isrt@gmail.com>
2024-04-22 08:14:40 -0500

    create booking

---
+++ frontend/src/app/bike/bike-form/bike-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bike-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './bike-form.component.html',
+  styleUrl: './bike-form.component.css'
+})
+export class BikeFormComponent {
+
+}
---
+++ frontend/src/app/booking/book-form/book-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-form.component.html',
+  styleUrl: './book-form.component.css'
+})
+export class BookFormComponent {
+
+}
