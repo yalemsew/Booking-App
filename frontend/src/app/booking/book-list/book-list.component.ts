6bbb640ce7be237f82a69cdafc5f0e21f30d9d53
Yalemsew Lashitaw Mekuriaw <yalem@Yalemsews-MacBook-Pro.local>
2024-04-23 07:59:24 -0500

    remove angular lib

--- frontend/src/app/booking/book-list/book-list.component.ts
+++ frontend/src/app/booking/book-list/book-list.component.ts
@@ -15,7 +15,6 @@ import { Router } from "@angular/router";
 })
 export class BookListComponent {
   http = inject(HttpClient);
-  // interceptor = inject(addTokenInterceptor);
 
   BookingList: Booking[] = [];
   #router = inject(Router);
